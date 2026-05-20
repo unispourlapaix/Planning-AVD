4476485495e31697077540d3ee1d1d6ced75fe7b
Emmanuel Payet <emmanuelpayet888@gmail.com>
2026-05-21 01:42:47 +0200

    Configure Firebase email authentication

--- auth-config.js
+++ auth-config.js
@@ -1,9 +1,12 @@
 window.PLANNING_AVD_AUTH = {
   firebaseConfig: {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    appId: ""
+    apiKey: "AIzaSyDdulxoZ0djfLHnwcRCDuLTFORclHy7QFk",
+    authDomain: "p-avd-1963b.firebaseapp.com",
+    projectId: "p-avd-1963b",
+    storageBucket: "p-avd-1963b.firebasestorage.app",
+    messagingSenderId: "93775651769",
+    appId: "1:93775651769:web:e36ee5e44affbee0370c68",
+    measurementId: "G-L8KQDLDJH2"
   },
   allowedEmails: []
 };
