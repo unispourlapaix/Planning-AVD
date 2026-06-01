234834d0f8a2c59091c32ba4634d1c9d464a9adc
Emmanuel Payet <emmanuelpayet888@gmail.com>
2026-06-02 00:33:43 +0200

    Load Gmail tab sharing fix

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { initPlanningShareButton } from "./modules/share-button.js?v=20260601";
+import { initPlanningShareButton } from "./modules/share-button.js?v=20260602-gmail-tab";
 
 globalThis.React = React;
 globalThis.ReactDOM = ReactDOM;
