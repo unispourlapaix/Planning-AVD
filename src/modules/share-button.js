2e961b7db675d06160cf445af94cc3da04a2241a
Emmanuel Payet <emmanuelpayet888@gmail.com>
2026-06-02 00:33:39 +0200

    Refresh Gmail sharing module

--- src/modules/share-button.js
+++ src/modules/share-button.js
@@ -1,7 +1,7 @@
 import { SHIFT_DEFS } from "./constants.js";
 import { buildSchedule, calculateHours } from "./scheduler-handover.js?v=20260601-saturday-morning";
 import { loadState } from "./storage.js";
-import { sharePlanningByEmail } from "./planning-share.js";
+import { sharePlanningByEmail } from "./planning-share.js?v=20260602-gmail-tab";
 
 const overrideKey = (year, month, day, shift) => `${year}-${month}-${day}-${shift}`;
 const applyOverrides = ({ schedule, overrides = {}, year, month }) => Object.fromEntries(
