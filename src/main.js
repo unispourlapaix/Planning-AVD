44a4fcafd223b8a1d9784230d49c5673d6b84171
Emmanuel Payet <emmanuelpayet888@gmail.com>
2026-06-02 21:59:22 +0200

    Load loop-safe duplicate labels

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { initPlanningShareButton } from "./modules/share-button.js?v=20260602-save-label-live";
 import { initPersonalTeamCalendar } from "./modules/personal-team-calendar.js?v=20260602";
-import { initVisualShiftLabels } from "./modules/visual-shift-labels.js?v=20260602-three-letter-doubles";
+import { initVisualShiftLabels } from "./modules/visual-shift-labels.js?v=20260602-three-letter-doubles-loop-fix";
 import { initPwaInstall } from "./modules/pwa-install.js?v=20260602";
 
 globalThis.React = React;
