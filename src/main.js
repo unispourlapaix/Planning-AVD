eb4f266a97537d26e4b831c7187f50c2b3a3dab6
Emmanuel Payet <emmanuelpayet888@gmail.com>
2026-06-02 22:22:57 +0200

    Restore auxiliary team calendar as module three

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { initVisualShiftLabels } from "./modules/visual-shift-labels.js?v=20260602-module-1-stacked";
 import { initPlanningShareButton } from "./modules/share-button.js?v=20260602-module-2-share";
+import { initPersonalTeamCalendar } from "./modules/personal-team-calendar.js?v=20260602-module-3-team-calendar-safe";
 
 globalThis.React = React;
 globalThis.ReactDOM = ReactDOM;
@@ -14,3 +15,4 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(h(App));
 initVisualShiftLabels();
 initPlanningShareButton();
+initPersonalTeamCalendar();
