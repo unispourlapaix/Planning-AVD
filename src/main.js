c5af58a086dcc48f195d916f618f464d07869518
Emmanuel Payet <emmanuelpayet888@gmail.com>
2026-06-02 21:38:52 +0200

    Enable installable web app mode

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { initPlanningShareButton } from "./modules/share-button.js?v=20260602-save-label-live";
 import { initPersonalTeamCalendar } from "./modules/personal-team-calendar.js?v=20260602";
 import { initVisualShiftLabels } from "./modules/visual-shift-labels.js?v=20260602-three-letter-doubles";
+import { initPwaInstall } from "./modules/pwa-install.js?v=20260602";
 
 globalThis.React = React;
 globalThis.ReactDOM = ReactDOM;
@@ -16,3 +17,4 @@ root.render(h(App));
 initPlanningShareButton();
 initPersonalTeamCalendar();
 initVisualShiftLabels();
+initPwaInstall();
