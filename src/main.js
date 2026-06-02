6063d2e7178d36b09e67f76c733beee1c7166328
Emmanuel Payet <emmanuelpayet888@gmail.com>
2026-06-02 21:32:22 +0200

    Load full visual shift labels

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { initPlanningShareButton } from "./modules/share-button.js?v=20260602-save-label-live";
 import { initPersonalTeamCalendar } from "./modules/personal-team-calendar.js?v=20260602";
+import { initVisualShiftLabels } from "./modules/visual-shift-labels.js?v=20260602";
 
 globalThis.React = React;
 globalThis.ReactDOM = ReactDOM;
@@ -14,3 +15,4 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(h(App));
 initPlanningShareButton();
 initPersonalTeamCalendar();
+initVisualShiftLabels();
