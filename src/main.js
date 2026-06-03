f6b0b48d1da40a10657141b29047dc30ca7a30bf
Emmanuel Payet <emmanuelpayet888@gmail.com>
2026-06-03 19:11:48 +0200

    Refresh auxiliary planning fallback module

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { initVisualShiftLabels } from "./modules/visual-shift-labels.js?v=20260602-module-1-stacked-week-fix";
 import { initPlanningShareButton } from "./modules/share-button.js?v=20260602-module-2-share";
-import { initPersonalTeamCalendar } from "./modules/personal-team-calendar.js?v=20260603-admin-like-personal-view";
+import { initPersonalTeamCalendar } from "./modules/personal-team-calendar.js?v=20260603-personal-fallback";
 import { initPwaInstall } from "./modules/pwa-install.js?v=20260602-module-4-install-safe";
 import { initMobileManualErgonomics } from "./modules/mobile-manual-ergonomics.js?v=20260602-module-10-action-row";
 import { initSettingsTools } from "./modules/settings-tools.js?v=20260602-module-9-settings-only";
