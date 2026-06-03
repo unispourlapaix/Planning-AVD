f4de8a640c1f103f88e8771ec8c587fcebae93b4
Emmanuel Payet <emmanuelpayet888@gmail.com>
2026-06-03 20:02:08 +0200

    Refresh shift time labels

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { initVisualShiftLabels } from "./modules/visual-shift-labels.js?v=20260602-module-1-stacked-week-fix";
+import { initVisualShiftLabels } from "./modules/visual-shift-labels.js?v=20260603-shift-times";
 import { initPlanningShareButton } from "./modules/share-button.js?v=20260602-module-2-share";
-import { initPersonalTeamCalendar } from "./modules/personal-team-calendar.js?v=20260603-personal-fallback";
+import { initPersonalTeamCalendar } from "./modules/personal-team-calendar.js?v=20260603-shift-times";
 import { initPwaInstall } from "./modules/pwa-install.js?v=20260602-module-4-install-safe";
 import { initMobileManualErgonomics } from "./modules/mobile-manual-ergonomics.js?v=20260602-module-10-action-row";
 import { initSettingsTools } from "./modules/settings-tools.js?v=20260602-module-9-settings-only";
@@ -11,7 +11,7 @@ globalThis.React = React;
 globalThis.ReactDOM = ReactDOM;
 
 const [{ default: App }, { h }] = await Promise.all([
-  import("./App.js?v=20260601-restored-valid"),
+  import("./App.js?v=20260603-shift-times"),
   import("./ui.js"),
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
