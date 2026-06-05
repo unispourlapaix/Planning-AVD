cf31967004d6293c58399a61d68b0e9b34af9a38
Emmanuel Payet <emmanuelpayet888@gmail.com>
2026-06-06 00:19:02 +0200

    Fix PWA install button flow

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { initVisualShiftLabels } from "./modules/visual-shift-labels.js?v=20260603-shift-times";
 import { initPlanningShareButton } from "./modules/share-button.js?v=20260602-module-2-share";
 import { initPersonalTeamCalendar } from "./modules/personal-team-calendar.js?v=20260603-shift-times";
-import { initPwaInstall } from "./modules/pwa-install.js?v=20260602-module-4-install-safe";
+import { initPwaInstall } from "./modules/pwa-install.js?v=20260606-install-button";
 import { initMobileManualErgonomics } from "./modules/mobile-manual-ergonomics.js?v=20260602-module-10-action-row";
 import { initSettingsTools } from "./modules/settings-tools.js?v=20260602-module-9-settings-only";
 
