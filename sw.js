0a2422da84c77173bc62c88f4202d963f3cfaa2f
Emmanuel Payet <emmanuelpayet888@gmail.com>
2026-06-06 00:46:57 +0200

    Scope service worker to Planning-AVD

--- sw.js
+++ sw.js
@@ -2,6 +2,12 @@ self.addEventListener("install", event => {
   self.skipWaiting();
 });
 
+const APP_SCOPE_PATH = new URL("./", self.location.href).pathname;
+
+const isPlanningAvdRequest = url =>
+  url.origin === self.location.origin
+  && (url.pathname === APP_SCOPE_PATH || url.pathname.startsWith(APP_SCOPE_PATH));
+
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys()
@@ -12,6 +18,6 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
   const url = new URL(event.request.url);
-  if (event.request.method !== "GET" || url.origin !== self.location.origin) return;
+  if (event.request.method !== "GET" || !isPlanningAvdRequest(url)) return;
   event.respondWith(fetch(event.request));
 });
