472e9ed3b3dcd8176c3f4e882e5f64d831733a8f
Emmanuel Payet <emmanuelpayet888@gmail.com>
2026-06-06 22:29:31 +0200

    Bump menu module cache keys

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { initVisualShiftLabels } from "./modules/visual-shift-labels.js?v=20260603-shift-times";
-import { initPlanningShareButton } from "./modules/share-button.js?v=20260602-module-2-share";
-import { initPersonalTeamCalendar } from "./modules/personal-team-calendar.js?v=20260606-private-display";
+import { initPlanningShareButton } from "./modules/share-button.js?v=20260607-modern-icons";
+import { initPersonalTeamCalendar } from "./modules/personal-team-calendar.js?v=20260607-modern-icons";
 import { initMobileManualErgonomics } from "./modules/mobile-manual-ergonomics.js?v=20260602-module-10-action-row";
 import { initSettingsTools } from "./modules/settings-tools.js?v=20260602-module-9-settings-only";
 import { initPwaInstall } from "./modules/pwa-install.js?v=20260606-direct-sw";
@@ -12,7 +12,7 @@ globalThis.React = React;
 globalThis.ReactDOM = ReactDOM;
 
 const [{ default: App }, { h }] = await Promise.all([
-  import("./App.js?v=20260603-shift-times"),
+  import("./App.js?v=20260607-modern-icons"),
   import("./ui.js"),
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
