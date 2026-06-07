9dae64eebcfc3b51b29fedd8b4087d571a914dc7
Emmanuel Payet <emmanuelpayet888@gmail.com>
2026-06-07 12:36:08 +0200

    Refresh Planning-AVD module cache keys

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { initVisualShiftLabels } from "./modules/visual-shift-labels.js?v=20260603-shift-times";
-import { initPlanningShareButton } from "./modules/share-button.js?v=20260607-modern-icons";
-import { initPersonalTeamCalendar } from "./modules/personal-team-calendar.js?v=20260607-modern-icons";
+import { initPlanningShareButton } from "./modules/share-button.js?v=20260607-weekend-one";
+import { initPersonalTeamCalendar } from "./modules/personal-team-calendar.js?v=20260607-personal-clean";
 import { initMobileManualErgonomics } from "./modules/mobile-manual-ergonomics.js?v=20260602-module-10-action-row";
-import { initPersonalMobileView } from "./modules/personal-mobile-view.js?v=20260606-personal-mobile";
+import { initPersonalMobileView } from "./modules/personal-mobile-view.js?v=20260607-personal-clean";
 import { initSettingsTools } from "./modules/settings-tools.js?v=20260602-module-9-settings-only";
 import { initPwaInstall } from "./modules/pwa-install.js?v=20260606-direct-sw";
 import { initPrivateDisplay } from "./modules/private-display.js?v=20260606-private-display";
@@ -13,7 +13,7 @@ globalThis.React = React;
 globalThis.ReactDOM = ReactDOM;
 
 const [{ default: App }, { h }] = await Promise.all([
-  import("./App.js?v=20260606-personal-mobile"),
+  import("./App.js?v=20260607-weekend-one"),
   import("./ui.js"),
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
