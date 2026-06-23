722c3588fdf6fd35a5d01aa13550348f3ca21219
Emmanuel Payet <emmanuelpayet888@gmail.com>
2026-06-23 23:50:37 +0200

    Bump app module cache version

--- src/main.js
+++ src/main.js
@@ -20,7 +20,7 @@ window.alert = message => nativeAlert(String(message)
   .replace("Publication impossible", "Sauvegarde impossible"));
 
 const [{ default: App }, { h }] = await Promise.all([
-  import("./App.js?v=20260622-team-tools"),
+  import("./App.js?v=20260623-firestore-cache"),
   import("./ui.js"),
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
